00272a0bfe51e53f916881953bc81e38a3b5f670
marcelo <marnoli_88@hotmail.com>
2022-05-06 00:30:16 -0300

    feito formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { NewMomentsComponent } from './components/pages/new-moments/new-moments.component';
+import { MomentFormComponent } from './components/moment-form/moment-form.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    NewMomentsComponent,
+    MomentFormComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/pages/new-moments/new-moments.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NewMomentsComponent } from './new-moments.component';
+
+describe('NewMomentsComponent', () => {
+  let component: NewMomentsComponent;
+  let fixture: ComponentFixture<NewMomentsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ NewMomentsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NewMomentsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
